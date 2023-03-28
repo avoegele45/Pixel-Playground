3ec5c58ad55c2f733c7183acf41326fcee648f36
Madison <maddiebilotta05@icloud.com>
2023-03-27 20:06:24 -0500

    Added a login page

---
+++ login-action.js
@@ -0,0 +1,11 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    location.href = 'home.html';
+})
