f4ff1f7ed6227e450e519e79a744af6562a40f05
nathanieltooley <gotablast99@gmail.com>
2023-03-28 22:16:45 -0500

    now uses dbproxy

--- js/login-controller.js
+++ js/login-controller.js
@@ -1,10 +1,13 @@
 import { CookieManager } from "./cookie-manager.js";
+import { DBProxy } from "./db.js";
 
 export class LoginController {
 
     static login(username, password) {
-        CookieManager.setCookieKey("username", username);
-        CookieManager.setCookieKey("password", password);
+        if (DBProxy.validateLogin(username, password)) {
+            CookieManager.setCookieKey("username", username);
+            CookieManager.setCookieKey("password", password);
+        }
 
         console.log(CookieManager.getCookieObject());
     }
