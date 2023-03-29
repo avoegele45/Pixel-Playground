a3e3f4ebbd82f58f1e2c0315c438f47e5c20e9f8
nathanieltooley <gotablast99@gmail.com>
2023-03-28 22:16:39 -0500

    added code to dbproxy

--- js/db.js
+++ js/db.js
@@ -1,9 +1,13 @@
 export class DBProxy {
-
+    static validateLogin(username, password) {
+        return MockDB.validateLogin(username, password)
+    }
 }
 
 class MockDB {
-
+    static validateLogin(username, password) {
+        return true;
+    }
 }
 
 class RealDB {
