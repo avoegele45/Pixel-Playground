4c7dc497b63ceb3a59e591223b2ee6ffb3ef3e50
Nathaniel Tooley <gotablast99@gmail.com>
2023-03-31 16:41:14 -0500

    added extra methods to dbproxy

--- js/db.js
+++ js/db.js
@@ -1,6 +1,17 @@
 export class DBProxy {
+
+    static _db = MockDB;
+
     static validateLogin(username, password) {
-        return MockDB.validateLogin(username, password)
+        return DBProxy._db.validateLogin(username, password);
+    }
+
+    static saveLoginInfo(username, password) {
+        DBProxy._db.saveLoginInfo(username, password);
+    }
+
+    static saveData(table, username, key, value) {
+        DBProxy._db.saveData(table, username, key, value);
     }
 }
 
