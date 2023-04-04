51eed49431fbebe117d7a6f2f031ce664ae8b825
avoegele45 <122381868+avoegele45@users.noreply.github.com>
2023-04-04 00:50:45 -0500

    Update audio-script.js
    fleshed out class more to make it handle all audio controls rather than some being in dashboard

--- js/audio-script.js
+++ js/audio-script.js
@@ -1,12 +1,13 @@
 import { DBProxy } from "./db.js";
 
-
+let audioSettings = document.getElementById("audio");
 
 class AudioSettingsSaver {
     constructor(tableName) {
         this.tableName = tableName;
         this.keyName = "audio"
     }
+    
     
     
     save(audioSettings) {
@@ -19,6 +20,31 @@ class AudioSettingsSaver {
         let username = DBProxy.getLoginInfo().username;
 
         return DBProxy.getData(this.tableName, username, this.keyName);
+    }
+
+    loadAudioSettings(muteButton){
+        window.addEventListener("load", (e)=> {
+            Mute.userAudio.get() == "true" ? audioSettings.mute = true : audioSettings.play();
+            Mute.userAudio.get() == "true" ? muteButton.innerHTML = "Play" : muteButton.innerHTML = "Mute";
+        });
+    }
+
+    toggleMute(muteButton){
+        muteButton.addEventListener("click", (e) => {
+            let userSettings;
+            if (audioSettings.muted === false) {
+                audioSettings.muted = true;
+                userSettings = "true";
+                muteButton.innerHTML = "Play";
+            }
+            else{
+                audioSettings.muted = false;
+                userSettings = "false";
+                muteButton.innerHTML = "Mute";
+                audioSettings.play();
+            }
+            this.save(userSettings);
+        });
     }
 
 }
