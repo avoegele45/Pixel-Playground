3bc7c757478566117b09cace51a9e48515b5c5a3
Madison <maddiebilotta05@icloud.com>
2023-04-16 16:25:14 -0500

    moved theme selection to seperate settings page

--- js/dashboard-scripts.js
+++ js/dashboard-scripts.js
@@ -28,36 +28,8 @@ Savers.UserAudio.loadAudioSettings(mutebutton);
 Savers.UserAudio.toggleMute(mutebutton);
 
 // Changing and managing dashboard themes:
-const standardBtn = document.getElementById('standard');
-const lightBtn = document.getElementById('light');
-const frogBtn = document.getElementById('frog');
-const tronBtn = document.getElementById('tron');
 const link = document.querySelector('link[rel="stylesheet"]');
 
 if (Savers.Theme.get() != null) {
   link.href = Savers.Theme.get();
 }
-
-standardBtn.addEventListener('click', function() {
-  link.href = 'styles/dashboard-styles.css';
-  Savers.Theme.save('styles/dashboard-styles.css');
-  Savers.InfoTheme.save('styles/info.css');
-});
-
-lightBtn.addEventListener('click', function() {
-  link.href = 'styles/retro-lightmode-style.css';
-  Savers.Theme.save('styles/retro-lightmode-style.css');
-  Savers.InfoTheme.save('styles/info-light.css');
-});
-
-frogBtn.addEventListener('click', function() {
-  link.href = 'styles/frog-theme-style.css';
-  Savers.Theme.save('styles/frog-theme-style.css');
-  Savers.InfoTheme.save('styles/info-frog.css');
-});
-
-tronBtn.addEventListener('click', function() {
-  link.href = 'styles/tron-theme-style.css';
-  Savers.Theme.save('styles/tron-theme-style.css');
-  Savers.InfoTheme.save('styles/info-tron.css');
-});
