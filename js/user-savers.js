d33a1eb7caebd2ccc1dfb6a887c0ac1bff22f6af
Madison <maddiebilotta05@icloud.com>
2023-04-16 14:11:33 -0500

    fixed theme buttons and saved in cookies

--- js/user-savers.js
+++ js/user-savers.js
@@ -19,8 +19,29 @@ class HighScoreSaver {
     }
 }
 
+class ThemeSaver {
+    constructor(tableName) {
+        this.tableName = tableName;
+        this.keyName = "dashtheme";
+    }
+
+    save(dashtheme) {
+        let username = DBProxy.getLoginInfo().username;
+
+        DBProxy.saveData(this.tableName, username, this.keyName, dashtheme);
+    }
+
+    get() {
+        let username = DBProxy.getLoginInfo().username;
+
+        return DBProxy.getData(this.tableName, username, this.keyName);
+    }
+}
+
 export class Savers {
     static Tetris = new HighScoreSaver("tetris");
     static Frogger = new HighScoreSaver("frogger");
     static Breakout = new HighScoreSaver("breakout");
+    static Theme = new ThemeSaver("themes");
 }
+
