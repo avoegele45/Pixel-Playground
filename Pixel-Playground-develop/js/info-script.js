94955409fe8286f9f0bbf435465153b9bf0da39b
avoegele45 <122381868+avoegele45@users.noreply.github.com>
2023-04-20 07:19:10 -0500

    Delete Pixel-Playground-develop directory

--- Pixel-Playground-develop/js/info-script.js
+++
@@ -1,7 +0,0 @@
-import { Savers } from "./user-savers.js";
-
-const link = document.querySelector('link[rel="stylesheet"]');
-
-if (Savers.InfoTheme.get() != null) {
-  link.href = Savers.InfoTheme.get();
-}
